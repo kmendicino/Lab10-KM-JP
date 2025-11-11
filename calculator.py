947c4dc27597ddb70ab1248015289a666e47f53c
kmendicino <kmendicino@ufl.edu>
2025-11-11 15:23:10 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -7,24 +7,31 @@ import math
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return a / b
 
-def log(a, b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid input for logarithm")
-    return math.log(b, a)
+def logarithm(a, base):
+    if a <= 0:
+        raise ValueError("Logarithm argument must be > 0")
+    if base <= 0 or base == 1:
+        raise ValueError("Invalid logarithm base")
+    return math.log(a, base)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
 
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
-
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
