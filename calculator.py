642c428307f79797be22020197a3735989b083f7
kmendicino <kmendicino@ufl.edu>
2025-11-11 15:18:56 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,30 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/kmendicino/Lab10-KM-JP
+# Partner 1: Kate Mendicino
+# Partner 2: Jingjing Peng
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
 
 
