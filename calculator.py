f1a1527f16e8d01208f5ba70db72bb017b151255
kmendicino <kmendicino@ufl.edu>
2025-11-11 15:24:30 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -7,25 +7,25 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return a / b
 
-def logarithm(a, base):
+def log(a, base):
     if a <= 0:
         raise ValueError("Logarithm argument must be > 0")
     if base <= 0 or base == 1:
         raise ValueError("Invalid logarithm base")
     return math.log(a, base)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
 
 def hypotenuse(a, b):
