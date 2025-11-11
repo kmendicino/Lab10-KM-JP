d4cc053b1153cd469af9904a1f6ba7a844ba7cc5
kmendicino <kmendicino@ufl.edu>
2025-11-11 15:48:29 -0500

    test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,11 @@
 # https://github.com/kmendicino/Lab10-KM-JP
 # Partner 1: Kate Mendicino
 # Partner 2: Jingjing Peng
-
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
+
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
@@ -15,7 +15,19 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(0, 0), 0)
-        self.assertEqual(subtract(3, 5), -2)
+        self.assertEqual(subtract(2, 5), -3)
+
+
+    def test_multiply(self):
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-1, 5), -5)
+        self.assertEqual(mul(0, 10), 0)
+
+    def test_divide(self):
+        self.assertEqual(div(6, 3), 2)
+        self.assertEqual(div(-10, 2), -5)
+        self.assertAlmostEqual(div(7, 2), 3.5)
+
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
@@ -24,23 +36,13 @@ class TestCalculator(unittest.TestCase):
     def test_logarithm(self):
         self.assertAlmostEqual(logarithm(8, 2), 3)
         self.assertAlmostEqual(logarithm(27, 3), 3)
-        self.assertAlmostEqual(logarithm(16, 2), 4)
+        self.assertAlmostEqual(logarithm(100, 10), 2)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
             logarithm(10, 1)
         with self.assertRaises(ValueError):
-            logarithm(10, -2)
-
-    def test_multiply(self):
-        self.assertEqual(mul(2, 3), 6)
-        self.assertEqual(mul(-1, 5), -5)
-        self.assertEqual(mul(0, 100), 0)
-
-    def test_divide(self):
-        self.assertEqual(div(6, 3), 2)
-        self.assertEqual(div(5, 2), 2.5)
-        self.assertEqual(div(-4, 2), -2)
+            logarithm(5, 0)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
@@ -50,4 +52,16 @@ class TestCalculator(unittest.TestCase):
 
     def test_hypotenuse(self):
         self.assertAlmostEqual(hypotenuse(3, 4), 5)
-        self.assertAlm
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17)
+
+    def test_sqrt(self):
+        self.assertAlmostEqual(square_root(9), 3)
+        self.assertAlmostEqual(square_root(0), 0)
+        with self.assertRaises(ValueError):
+            square_root(-4)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
